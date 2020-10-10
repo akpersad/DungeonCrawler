bb2cf7d24027cfa8f1f3199b46a9f693a5c1cb5f
Andrew Persad <apersad@deloitte.com>
2020-10-09 23:23:05 -0400

    Title Screen styling

--- src/components/startScreen/startScreen.jsx
+++ src/components/startScreen/startScreen.jsx
@@ -1,14 +1,16 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import TitleCard from "../titleCard/titleCard";
 import CharacterSelect from "../characterSelect/characterSelect";
 // import PropTypes from 'prop-types';
 
 class StartScreen extends Component {
 	render() {
 		return (
-			<>
+			<div className="start-screen_container">
+				<TitleCard />
 				<CharacterSelect />
-			</>
+			</div>
 		);
 	}
 }
